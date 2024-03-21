9e18b658ab45696cbbb2dda3104409d3eba732b5
Marvel paul <djmarvel@yahoo.com>
2024-03-20 21:15:24 -0700

    debbugged task 6 and checked if it is a file storage before adding getter attribute cities

--- models/review.py
+++ models/review.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
