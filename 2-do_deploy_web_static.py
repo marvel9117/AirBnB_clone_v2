e265798f24b072f60b01c5eca303c031b9fe1bad
Marvel paul <djmarvel@yahoo.com>
2024-04-04 14:17:17 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['52.91.102.19', '54.84.252.70']
 
 
 def do_deploy(archive_path):
