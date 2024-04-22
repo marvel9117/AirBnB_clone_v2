3266325d1087637fb3302114da771c6019855604
Marvel paul <djmarvel@yahoo.com>
2024-04-22 05:32:58 -0700

    7-dump.sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,10 +23,10 @@ def states_list():
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port='5000')
